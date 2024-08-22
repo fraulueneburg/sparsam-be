5d5ba49746e0773ddeada89f963e489006c9f8de
fraulueneburg <github@fraulueneburg.de>
2024-08-22 23:16:13 +0200

    added update settings routes

--- routes/budget.routes.js
+++ routes/budget.routes.js
@@ -3,11 +3,13 @@ const DailyExpenses = require('../models/DailyExpenses.model')
 const router = require('express').Router()
 const { isAuthenticated } = require('../middlewares/jwt.auth')
 
+// ---------------------------------------------------------------------------------------------------
+
 // BUDGET
 
 router.get('/', isAuthenticated, async (req, res) => {
 	const userId = req.payload._id
-	const foundMonthlyBudget = await MonthlyBudget.find({ user: userId })
+	const foundMonthlyBudget = await MonthlyBudget.findOne({ user: userId })
 	const foundDailyExpenses = await DailyExpenses.find({ user: userId })
 	res.json({ respMonthlyBudget: foundMonthlyBudget, respDailyExpenses: foundDailyExpenses })
 })
@@ -16,7 +18,7 @@ router.get('/', isAuthenticated, async (req, res) => {
 
 router.get('/settings', isAuthenticated, async (req, res) => {
 	const userId = req.payload._id
-	const foundMonthlyBudget = await MonthlyBudget.find({ user: userId })
+	const foundMonthlyBudget = await MonthlyBudget.findOne({ user: userId })
 	res.json({ respMonthlyBudget: foundMonthlyBudget })
 })
 
@@ -26,7 +28,7 @@ router.post('/create', isAuthenticated, async (req, res) => {
 	try {
 		const userId = req.payload._id
 		const newBudgetData = req.body
-		const budget = await MonthlyBudget.find({ user: userId })
+		const budget = await MonthlyBudget.findOne({ user: userId })
 
 		if (budget.length === 0) {
 			const newBudget = await MonthlyBudget.create({
@@ -34,9 +36,9 @@ router.post('/create', isAuthenticated, async (req, res) => {
 				currency: newBudgetData.currency,
 				earnings: newBudgetData.earnings,
 				expenses: newBudgetData.expenses,
-				spendingCategories: newBudgetData.spendingCategories,
+				categories: newBudgetData.categories,
 			})
-			res.status(201).json(newBudget)
+			console.log('BE: New Budget created ', newBudget)
 		} else {
 			const updatedBudget = await MonthlyBudget.findByIdAndUpdate(
 				budget[0]._id,
@@ -44,17 +46,286 @@ router.post('/create', isAuthenticated, async (req, res) => {
 					currency: newBudgetData.currency,
 					earnings: newBudgetData.earnings,
 					expenses: newBudgetData.expenses,
-					spendingCategories: newBudgetData.spendingCategories,
+					categories: newBudgetData.categories,
 				},
 				{ new: true }
 			)
 			res.status(201).json(updatedBudget)
-			res.redirect(`/budget`)
 		}
 	} catch (err) {
 		console.log(err)
 	}
 })
+
+// UPDATE CURRENCY
+
+router.post('/currency/update', isAuthenticated, async (req, res) => {
+	try {
+		const userId = req.payload._id
+		const newCurrency = req.body.currency
+		const budget = await MonthlyBudget.findOne({ user: userId })
+
+		if (budget) {
+			budget.currency = newCurrency
+			const updatedBudget = await budget.save()
+			return res.status(200).json(updatedBudget)
+		} else {
+			const newBudget = await MonthlyBudget.create({
+				user: userId,
+				currency: newCurrency,
+			})
+			return res.status(201).json(newBudget)
+		}
+	} catch (err) {
+		console.log(err)
+		return res.status(500).json({ message: 'BE Error while updating currency' })
+	}
+})
+
+// ---------------------------------------------------------------------------------------------------
+
+// ADD NEW EARNING
+
+router.post('/earnings/add', isAuthenticated, async (req, res) => {
+	try {
+		const userId = req.payload._id
+		const newEarningsArr = req.body.earnings
+		const budget = await MonthlyBudget.findOne({ user: userId })
+
+		if (budget) {
+			budget.earnings = newEarningsArr
+			const updatedEarnings = await budget.save()
+			return res.status(200).json(updatedEarnings)
+		} else {
+			const newBudget = await MonthlyBudget.create({
+				user: userId,
+				earnings: newEarningsArr,
+			})
+			return res.status(201).json(newBudget)
+		}
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({ message: 'BE Error while adding new earning' })
+	}
+})
+
+// UPDATE EARNING
+
+router.post('/earnings/update', isAuthenticated, async (req, res) => {
+	try {
+		const userId = req.payload._id
+		const updatedData = req.body.updatedEarning
+		const earningId = updatedData._id
+
+		const budget = await MonthlyBudget.findOneAndUpdate(
+			{ user: userId, 'earnings._id': earningId },
+			{
+				$set: {
+					'earnings.$.name': updatedData.name,
+					'earnings.$.amount': updatedData.amount,
+				},
+			},
+			{ new: true }
+		)
+
+		if (!budget) {
+			return res.status(404).json({ message: 'Budget or earning not found' })
+		}
+
+		res.status(200).json(budget)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({ message: 'BE Error while updating existing earning' })
+	}
+})
+
+// DELETE EARNING
+
+router.delete('/earnings/delete/:earningId', isAuthenticated, async (req, res) => {
+	try {
+		const userId = req.payload._id
+		const earningId = req.params.earningId
+		const budget = await MonthlyBudget.findOneAndUpdate(
+			{ user: userId },
+			{
+				$pull: {
+					earnings: { _id: earningId },
+				},
+			},
+			{ new: true }
+		)
+		if (!budget) {
+			return res.status(404).json({ message: 'Budget or earning not found' })
+		}
+		res.status(200).json(budget)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({ message: 'BE Error while deleting existing earning' })
+	}
+})
+
+// ---------------------------------------------------------------------------------------------------
+
+// ADD NEW EXPENSE
+
+router.post('/expenses/add', isAuthenticated, async (req, res) => {
+	try {
+		const userId = req.payload._id
+		const newExpensesArr = req.body.expenses
+		const budget = await MonthlyBudget.findOne({ user: userId })
+
+		if (budget) {
+			budget.expenses = newExpensesArr
+			const updatedExpenses = await budget.save()
+			return res.status(200).json(updatedExpenses)
+		} else {
+			const newBudget = await MonthlyBudget.create({
+				user: userId,
+				expenses: newExpensesArr,
+			})
+			return res.status(201).json(newBudget)
+		}
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({ message: 'BE Error while adding new expense' })
+	}
+})
+
+// UPDATE EXPENSE
+
+router.post('/expenses/update', isAuthenticated, async (req, res) => {
+	try {
+		const userId = req.payload._id
+		const updatedData = req.body.updatedExpense
+		const expenseId = updatedData._id
+
+		const budget = await MonthlyBudget.findOneAndUpdate(
+			{ user: userId, 'expenses._id': expenseId },
+			{
+				$set: {
+					'expenses.$.name': updatedData.name,
+					'expenses.$.amount': updatedData.amount,
+				},
+			},
+			{ new: true }
+		)
+
+		if (!budget) {
+			return res.status(404).json({ message: 'Budget or expense not found' })
+		}
+
+		res.status(200).json(budget)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({ message: 'BE Error while updating existing expense' })
+	}
+})
+
+// DELETE EXPENSE
+
+router.delete('/expenses/delete/:expenseId', isAuthenticated, async (req, res) => {
+	try {
+		const userId = req.payload._id
+		const expenseId = req.params.expenseId
+		const budget = await MonthlyBudget.findOneAndUpdate(
+			{ user: userId },
+			{
+				$pull: {
+					expenses: { _id: expenseId },
+				},
+			},
+			{ new: true }
+		)
+		if (!budget) {
+			return res.status(404).json({ message: 'Budget or expense not found' })
+		}
+		res.status(200).json(budget)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({ message: 'BE Error while deleting existing expense' })
+	}
+})
+
+// ---------------------------------------------------------------------------------------------------
+
+// ADD NEW CATEGORY
+
+router.post('/categories/add', isAuthenticated, async (req, res) => {
+	try {
+		const userId = req.payload._id
+		const newCategoriesArr = req.body.categories
+		const budget = await MonthlyBudget.findOne({ user: userId })
+
+		if (budget) {
+			budget.categories = newCategoriesArr
+			const updatedCategories = await budget.save()
+			return res.status(200).json(updatedCategories)
+		} else {
+			const newBudget = await MonthlyBudget.create({
+				user: userId,
+				categories: newCategoriesArr,
+			})
+			return res.status(201).json(newBudget)
+		}
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({ message: 'BE Error while adding new category' })
+	}
+})
+
+// UPDATE CATEGORY
+
+router.post('/categories/update', isAuthenticated, async (req, res) => {
+	try {
+		const userId = req.payload._id
+		const updatedData = req.body.updatedCategory
+		const categoryId = updatedData._id
+		const budget = await MonthlyBudget.findOneAndUpdate(
+			{ user: userId, 'categories._id': categoryId },
+			{
+				$set: {
+					'categories.$.name': updatedData.name,
+					'categories.$.colour': updatedData.colour,
+				},
+			},
+			{ new: true }
+		)
+		if (!budget) {
+			return res.status(404).json({ message: 'Budget or category not found' })
+		}
+		res.status(200).json(budget)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({ message: 'BE Error while updating existing category' })
+	}
+})
+
+// DELETE CATEGORY
+
+router.delete('/categories/delete/:categoryId', isAuthenticated, async (req, res) => {
+	try {
+		const userId = req.payload._id
+		const categoryId = req.params.categoryId
+		const budget = await MonthlyBudget.findOneAndUpdate(
+			{ user: userId },
+			{
+				$pull: {
+					categories: { _id: categoryId },
+				},
+			},
+			{ new: true }
+		)
+		if (!budget) {
+			return res.status(404).json({ message: 'Budget or category not found' })
+		}
+		res.status(200).json(budget)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({ message: 'BE Error while deleting existing category' })
+	}
+})
+
+// ---------------------------------------------------------------------------------------------------
 
 // ADD DAILY EXPENSE
 
