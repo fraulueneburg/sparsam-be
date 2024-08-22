f57bd4ac590f0f438fb0a8e34b6ba278e667cc4c
fraulueneburg <github@fraulueneburg.de>
2024-08-22 23:15:00 +0200

    updated budget model

--- models/MonthlyBudget.model.js
+++ models/MonthlyBudget.model.js
@@ -3,11 +3,16 @@ const { Schema, model } = require('mongoose')
 const monthlyBudgetSchema = new Schema(
 	{
 		user: { type: Schema.Types.ObjectId, ref: 'user' },
-		currency: { type: String, required: true, default: '€' },
+		currency: {
+			name: { type: String },
+			code: { type: String },
+			symbol: { type: String },
+			space_between_amount_and_symbol: { type: Boolean },
+			decimal_digits: { type: Number },
+		},
 		earnings: [{ name: { type: String }, amount: { type: Number } }],
 		expenses: [{ name: { type: String }, amount: { type: Number } }],
-		spendingCategories: [{ type: String }],
-		//spendingCategories: [{ name: { type: String }, color: { name: { type: String }, hue: { type: String } } }],
+		categories: [{ name: { type: String }, colour: { type: String } }],
 	},
 	{
 		timestamps: true,
