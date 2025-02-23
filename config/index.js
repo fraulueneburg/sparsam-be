d375cd15bb1f3d3253eece65d8727f5737b5a2ef
fraulueneburg <github@fraulueneburg.de>
2025-02-23 22:12:48 +0100

    removed redundant CORS options

--- config/index.js
+++ config/index.js
@@ -16,8 +16,7 @@ const cors = require('cors')
 const FRONTEND_URL = process.env.ORIGIN || 'http://localhost:3000'
 
 // Middleware configuration
-const app = express();
-app.options('*', cors())
+const app = express()
 
 module.exports = (app) => {
 	// Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
@@ -30,7 +29,6 @@ module.exports = (app) => {
 			origin: [FRONTEND_URL],
 			methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 			allowedHeaders: ['Content-Type', 'Authorization'],
-			credentials: true,
 		})
 	)
 
